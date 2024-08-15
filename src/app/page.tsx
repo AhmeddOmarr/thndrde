842305d9938c389de19bc8c09ec54f8199c25195
Ahmeddomarr <ahmed_omar358@icloud.com>
2024-08-15 12:29:40 +0300

    feat:newcomp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import type { FormProps } from 'antd';
 import {Checkbox, Form, Input } from 'antd';
-import Antform from "./components/login";
+import Antform from "./components/Login";
 import Fbutton from "./components/Button";
 import FirstC from "./components/FirstC";
 
